ad1df64c1e0c8a58a2225ae7dc41c61cc209b4e8
SBDY <david@SBDYs-MacBook-Pro.local>
2023-01-09 15:46:02 +0900

    Initial commit

---
+++ stats/views.py
@@ -0,0 +1,47 @@
+from django.shortcuts import render 
+
+posts = [
+    {
+        "author": "CoreyMs",
+        "title": "Blog Post 1",
+        "content": "UFC Lightweight Fighters",
+        "date_posted": "August 27, 2018"
+    },
+    {
+        "author": "Yooniverse",
+        "title": "Blog Post 2",
+        "content": "UFC Featherweight Fighters",
+        "date_posted": "August 28, 2018"
+    },
+    { "name": "Conor McGregor",
+      "nickname": "The Notorious",
+      "record": {"wins": 22,
+                 "losses": 5,
+                 "draws": 0
+                },
+      "height": "5'9",
+      "weight": 155,
+      "reach": 74,
+      "stance": "Southpaw",
+      "division": "Lightweight",
+      "age": 32,
+      "striking_accuracy": 55.0,
+      "grappling_accuracy": 48.0,
+      "striking_defense": 66.0,
+      "takedown_defense": 70.0,
+      "knockout_ratio": 0.59,
+      "submission_ratio": 0.18
+    }
+]
+
+def home(request):
+    context = {
+        "posts": posts
+    }
+    return render(request, "stats/home.html", context)
+
+def about(request):
+    return render(request, "stats/about.html", {"title": "About"})
+
+def fighters(request):
+    return render(request, "stats/fighters.html", {"title": "Fighters"})
